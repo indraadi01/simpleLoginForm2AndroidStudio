de1ae9fd8dd2411ddd468a08cf126a23c3d4a01f
indraadi01 <mindraadikusuma@mail.ugm.ac.id>
2023-10-15 20:30:47 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Acara 2 PGPB_M. Indra_21614"
+include(":app")
+ 
